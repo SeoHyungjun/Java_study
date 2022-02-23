b8fdc2d0d17be41d5e126aa57df107351054413e
SeoHyungjun <wewwe96@naver.com>
2022-02-23 18:08:00 +0900

    FEAT: Practicing instances using the new keyword and constructor
    Default constructor and constructor overloading practice

--- Java_Programming_Basic/chapter5/src/classpart/Student.java
+++ Java_Programming_Basic/chapter5/src/classpart/Student.java
@@ -6,6 +6,17 @@ public class Student {
     int grade;
     String address;
 
+    // 생성자는 처음 객체를 생성할 때 해야하는 일을 정의
+    // 생성자를 선언하지 않으면 default 생성자가 사용됨(실제로 선언 안해도)
+    // 그러나 다른 생성자가 있을때, default 생성자를 사용하고싶으면 overloading해서 사용해야한다.
+    public Student() {}
+
+    // 만약 생성할 때 id와 name을 받아서 생성할 경우
+    public Student(int id, String name) {
+        studentID = id;
+        studentName = name;
+    }
+
     public void showStudentInfo() {
         System.out.println(studentName + "," + address);
     }
@@ -20,11 +31,19 @@ public class Student {
 
     /*
     public static void main(String[] args) {
-        Student studenLee = new Student();
-        studenLee.studentName = "이순신";
-        studenLee.address = "서울시 서초구 서초동";
+        // JVM이 main을 먼저 호출
+        Student studentLee = new Student();
+        studentLee.studentName = "이순신";
+        studentLee.studentID = 100;
+        studentLee.address = "서울시 영등포구 여의도동";
 
-        studenLee.showStudentInfo();
+        Student studentKim = new Student();
+        studentKim.studentName = "김유신";
+        studentKim.studentID = 101;
+        studentKim.address = "미국 산호세";
+
+        studentLee.showStudentInfo();
+        studentKim.showStudentInfo();
     }
     */
 }
--- Java_Programming_Basic/chapter5/src/classpart/StudentTest.java
+++ Java_Programming_Basic/chapter5/src/classpart/StudentTest.java
@@ -2,10 +2,22 @@ package classpart;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student studentLee = new Student();
-        studentLee.studentName = "이순신";
-        studentLee.address = "서울시 서초구 서초동";
+        // JVM이 main을 먼저 호출
+        Student studentLee = new Student(100, "이순신");
+        studentLee.address = "서울시 영등포구 여의도동";
+
+        Student studentKim = new Student();
+        studentKim.studentName = "김유신";
+        studentKim.studentID = 101;
+        studentKim.address = "미국 산호세";
 
         studentLee.showStudentInfo();
+        studentKim.showStudentInfo();
+
+        // 인스턴스를 출력하면 32bit의 heap영역 address가 나온다.
+        // 인스턴스를 생성하면 실제로 heap영역에 데이터를 저장할 수 있는 공간을 할당하고,
+        // stack 영역에 heap 영역의 주소값을 나타내는 변수를 생성한다 (ex) studentLee, studentKim) )
+        System.out.println(studentLee);
+        System.out.println(studentKim);
     }
 }
